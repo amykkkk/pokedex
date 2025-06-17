26852a8c6027a839f402593ddaa6cf8604f56167
kimayeong <kimayeong@valofe.com>
2025-06-17 17:13:37 +0900

    로딩/에러 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,25 @@ import TypeFilter from "@/components/TypeFilter";
 export default function Home() {
   const [pokemonList, setPokemonList] = useState([]);
   const [type, setType] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = type
-        ? await fetchPokemonByType(type)
-        : await fetchAllPokemon();
+      setLoading(true);
+      setError("");
 
-      setPokemonList(data);
+      try {
+        const data = type
+          ? await fetchPokemonByType(type)
+          : await fetchAllPokemon();
+
+        setPokemonList(data);
+      } catch (err) {
+        setError("데이터를 불러오는데 실패...😭");
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchData();
@@ -25,15 +36,21 @@ export default function Home() {
     <main className="p-6">
       <h1 className="mb-4 text-2xl font-bold">🔥 Search</h1>
       <TypeFilter onSelect={setType} />
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-        {pokemonList.map((p: any) => (
-          <PokemonCard
-            key={p.name}
-            name={p.name}
-            image={`https://img.pokemondb.net/sprites/home/normal/${p.name}.png`}
-          />
-        ))}
-      </div>
+
+      {loading && <p className="text-gray-500">로딩 중입니다...🌀</p>}
+      {error && <p className="text-red-500">{error}</p>}
+
+      {!loading && !error && (
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+          {pokemonList.map((p: any) => (
+            <PokemonCard
+              key={p.name}
+              name={p.name}
+              image={`https://img.pokemondb.net/sprites/home/normal/${p.name}.png`}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
