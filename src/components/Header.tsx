51a4597732ae4a31d7d1d9cf172e493fc53adc6b
kimayeong <kimayeong@valofe.com>
2025-07-01 19:00:08 +0900

    다크모드 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,28 @@
 "use client";
 
+import { setCookie } from "@/utils/cookie";
+import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 
-export default function Header() {
-  const [theme, setTheme] = useState("light");
+export default function Header({ isTheme }: { isTheme: string }) {
+  const [theme, setTheme] = useState(isTheme);
 
   useEffect(() => {
+    setCookie("theme", theme);
+
     document.documentElement.classList.toggle("dark", theme === "dark");
   }, [theme]);
 
   return (
-    <div className={theme}>
-      <h1 className="text-banana mb-8 text-center text-4xl font-black">
-        🔥 Pokédex
-      </h1>
+    <div className="flex items-center justify-between px-6 py-4">
+      <h1 className="text-banana mb-8 text-center text-xl">🔥 Pokédex</h1>
       <button
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        className="p-2"
+        className={`border-border bg-bg relative h-5.5 w-10 rounded-full border transition-all duration-300 dark:bg-zinc-700`}
       >
-        {theme === "dark" ? "🌞 라이트모드" : "🌙 다크모드"}
+        <span className="text-text flex h-4.5 w-4.5 items-center justify-center rounded-full bg-white p-0.5 transition-all duration-300 dark:translate-x-5 dark:bg-black dark:text-white">
+          {theme === "light" ? <Sun /> : <Moon />}
+        </span>
       </button>
     </div>
   );
